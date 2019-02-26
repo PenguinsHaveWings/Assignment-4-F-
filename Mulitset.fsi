238679aa065baffeea1afe26f21bacdc072a8bbd
Anton Tobias Jensen <antj@itu.dk>
2019-02-26 15:49:09 +0100

    hihi

--- Mulitset.fsi
+++ Mulitset.fsi
@@ -6,7 +6,7 @@
     val isEmpty : MultiSet<'a> -> bool
     val size : MultiSet<'a> -> uint32
     val contains : 'a -> MultiSet<'a> -> bool
-    //val numItems : 'a -> MultiSet<'a> -> uint32
+    val numItems : 'a -> MultiSet<'a> -> uint32
     val add : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a>
     val addSingle : 'a -> MultiSet<'a> -> MultiSet<'a>
     val remove : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> 
@@ -14,7 +14,7 @@
     //val fold : ('a -> 'b -> uint32 -> 'a) -> 'a -> MultiSet<'b> -> 'a 
     //val foldBack : ('a -> uint32 -> 'b -> 'b) -> MultiSet<'a> -> 'b -> 'b
     //val map : ('a -> 'b) -> MultiSet<'a> -> MultiSet<'b>
-    //val ofList : 'a list -> MultiSet<'a>
+    val ofList : 'a list -> MultiSet<'a>
     //val toList : MultiSet<'a> -> 'a list
     val union : MultiSet<'a> -> MultiSet<'a> -> MultiSet<'a>
     val subtract : MultiSet<'a> -> MultiSet<'a> -> MultiSet<'a>
--- MultiSet.fs
+++ MultiSet.fs
@@ -4,9 +4,9 @@
 
     let empty = MS (Map.empty)
     let isEmpty (MS(m)) = Map.isEmpty m
-    let size (MS(m)) = uint32(Map.count m)
+    let size (MS(m)) = uint32(Map.count m) //TODO count values insted of number of keys
     let contains a (MS(m)) = Map.containsKey a m
-    let numItems a (MS(m)) = Map.tryFind(a)
+    let numItems a (MS(m)) = (Map.tryFind(a))
 
     let add a u (MS(m)) = 
         if Map.containsKey a m 
@@ -32,15 +32,15 @@
 
     let removeSingle a (MS(m)) = remove a 1u (MS(m))
 
-    let fold f = f //TODO Make fold
+    let fold f acc (MS(m)) = MS(Map.fold f acc m)
 
-    let foldBack f = f //TODO Make fold back function
+    let foldBack f acc (MS(m)) = MS(Map.foldBack f acc m)
 
-    let map m = m //TODO make map
+    let map mf (MS(m)) = MS(Map.map mf m)
 
-    let ofList l = Map.ofList l
+    let ofList l = List.fold (fun acc element -> addSingle element acc) empty l
 
-    let toList (MS(m)) = Map.toList
+    let toList (MS(m)) = (Map.toList m)
 
     let union (MS(s1)) (MS(s2)) = MS(Map(Seq.concat [ (Map.toSeq s1) ; (Map.toSeq s2) ]))
 
@@ -52,5 +52,5 @@
             |> Map.partition (fun _ value -> value > 0u)
         MS positiveCounts
 
-    let intersection s = s //TODO intersect
+    let intersection l = l
 
